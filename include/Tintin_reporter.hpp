c99df6917d715259420f982f970b0860705b10e7
mounadi05 <mounadi2015@gmail.com>
2025-07-11 18:32:24 -0400

    Add Tintin_reporter logging class - Implements singleton pattern for logging Supports INFO, ERROR, and LOG levels

---
+++ include/Tintin_reporter.hpp
@@ -0,0 +1,33 @@
+#ifndef TINTIN_REPORTER_HPP
+#define TINTIN_REPORTER_HPP
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fstream>
+#include <cstring>
+#include <stdexcept>
+#include <fcntl.h>
+#include <sys/file.h>
+#include <chrono>
+#include <iomanip>
+#include <stdexcept>
+#include <memory>
+class Tintin_reporter {
+public:
+    static Tintin_reporter& instance(); 
+    enum class Level { INFO, ERROR, LOG };
+    Tintin_reporter(const Tintin_reporter& other);
+    Tintin_reporter& operator=(const Tintin_reporter& other);
+    void log(const std::string& message, Level level);
+    ~Tintin_reporter();
+    
+private:
+    Tintin_reporter();
+    std::string get_timestamp();
+    std::ofstream log_file_;
+    void check_directory();
+    static constexpr const char* PATH = "/var/log/matt_daemon/matt_daemon.log";
+};
+
+#endif
